a978fad2c5bdcd84d964cee4d9c595a2c5acfd71
Matthew Frost <padatika@yahoo.com>
2015-08-30 20:43:13 -0400

    print header

--- PvPAudit.lua
+++ PvPAudit.lua
@@ -3,6 +3,8 @@ SLASH_PVPAUDIT2 = "/pa"
 
 local eventFrame = nil
 
+local TARGET = "target"
+
 local achievements = {
 	[1174] = "Arena Master",
 	[2091] = "Gladiator",
@@ -14,13 +16,18 @@ local function colorPrint(msg)
   print("|cffb2b2b2" .. msg)
 end
 
+local function classColorPrint(msg, class)
+	local color = string.format("|c%s", RAID_CLASS_COLORS[class]["colorStr"])
+  print(color .. msg)
+end
+
 local function errorPrint(err)
   print("|cffff0000" .. err)
 end
 
 local function audit()
-	local success = SetAchievementComparisonUnit("target")
-	local canInspect = CanInspect("target", false)
+	local success = SetAchievementComparisonUnit(TARGET)
+	local canInspect = CanInspect(TARGET, false)
 
 	if success and canInspect then
 		eventFrame:RegisterEvent("INSPECT_ACHIEVEMENT_READY")
@@ -29,11 +36,26 @@ local function audit()
 	end
 end
 
-local function onInspectReady()
+local function printHeader()
+	local name, realm = UnitName(TARGET)
+	if realm == nil then realm = "" end
+	local _, localeIndependentClass = UnitClass(TARGET)
+	local str = string.format("PvPAudit for %s %s", name, realm)
+
+	classColorPrint(str, localeIndependentClass)
+end
+
+local function printAchievements()
 	for k, v in pairs(achievements) do
 	  local completed = GetAchievementComparisonInfo(k)
 	  if completed then print(v) end
 	end
+end
+
+local function onInspectReady()
+	printHeader()
+	-- TODO PRINT CR
+	printAchievements()
 
 	eventFrame:UnregisterEvent("INSPECT_ACHIEVEMENT_READY")
 	ClearAchievementComparisonUnit()
