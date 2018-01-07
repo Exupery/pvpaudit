a021308067329a37c8eea9c3c77b7d5689b8fd1e
Matthew Frost <exupery@users.noreply.github.com>
2018-01-06 19:52:09 -0500

    added tabs to select bracket

--- ArenaHistoryViewer.lua
+++ ArenaHistoryViewer.lua
@@ -1,16 +1,39 @@
+local BRACKETS = { "2v2", "3v3" }
+
 local arenaDb = nil
 local eventFrame = nil
 
 local viewer = CreateFrame("Frame", "PvPAuditHistoryViewer", UIParent, "BasicFrameTemplateWithInset")
 viewer:SetClampedToScreen(true)
 viewer:SetMovable(true)
-viewer:Hide()
+viewer:EnableMouse(true)
+viewer:SetAlpha(0.8)
+
+local bracketTabs = CreateFrame("Frame", "PvPAuditBracketTabs", viewer)
 
 local function showViewer()
   viewer:Show()
 end
 
 local function eventHandler(self, event, unit, ...)
+end
+
+local function selectBracket(tab)
+  PanelTemplates_SetTab(bracketTabs, tab:GetID())
+end
+
+local function drawBracketTabs()
+  local xOffset = 0
+  for i, bracket in pairs(BRACKETS) do
+    local tab = CreateFrame("Button", bracketTabs:GetName().."Tab"..i, bracketTabs, "CharacterFrameTabButtonTemplate")
+    tab:SetID(i)
+    tab:SetText(bracket)
+    tab:SetPoint("TOPLEFT", viewer, "BOTTOMLEFT", xOffset, 0)
+    tab:SetScript("OnClick", selectBracket)
+    xOffset = tab:GetWidth() - 15
+  end
+  PanelTemplates_SetNumTabs(bracketTabs, table.getn(BRACKETS))
+  PanelTemplates_SetTab(bracketTabs, 1)
 end
 
 local function drawMainFrame()
@@ -24,6 +47,10 @@ local function drawMainFrame()
   viewer:SetScript("OnDragStart", viewer.StartMoving)
   viewer:SetScript("OnDragStop", viewer.StopMovingOrSizing)
   viewer:SetPoint("CENTER")
+
+  drawBracketTabs()
+
+  viewer:Hide()
 end
 
 function PvPAuditLoadViewerModule()
